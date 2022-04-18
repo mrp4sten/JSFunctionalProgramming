e870835b9f75de91022421cb9bfd11ad104fcf55
mrp4sten <mauricio.pasten.martinez@gmail.com>
2022-04-17 19:36:34 -0500

    Native filter

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const dollars = ["32$", "15$", "12$", "17$", "20$"];
 //map
 let prices = [];
 prices = dollars.map((dollar) => Number(dollar.slice(0, dollar.length - 1)));
-console.log(prices);
+console.log(`Prices: ${prices}`);
 
 //filter
-
+let expensive = [];
+for (const price of prices) {
+	if (price >= 20) {
+		expensive.push(price);
+	}
+}
+console.log(`Expensive prices: ${expensive}`);
 
 //reduce
 
