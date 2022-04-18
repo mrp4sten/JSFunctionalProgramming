2ae1cac3d5f442a36ed43af9f40f39b5d8291617
mrp4sten <mauricio.pasten.martinez@gmail.com>
2022-04-17 19:31:08 -0500

    map crack

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const dollars = ["32$", "15$", "12$", "17$", "20$"];
 
 //map
 let prices = [];
-for (let dollar of dollars) {
-	prices.push(Number(dollar.slice(0, dollar.length - 1)));
-}
+prices = dollars.map((dollar) => Number(dollar.slice(0, dollar.length - 1)));
 console.log(prices);
 
 //filter
