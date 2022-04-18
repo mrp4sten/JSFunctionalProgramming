c1db1a34d4269bb5b9b09c28c80f34b1be3dea5d
mrp4sten <mauricio.pasten.martinez@gmail.com>
2022-04-17 19:48:35 -0500

    Chaining

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 const dollars = ["32$", "15$", "12$", "17$", "20$"];
 
-//map
-let prices = [];
-prices = dollars.map((dollar) => Number(dollar.slice(0, dollar.length - 1)));
-console.log(`Prices: ${prices}`);
+// Chaining
+sum = dollars
+	.map(dollar => Number(dollar.slice(0, dollar.length - 1)))
+	.filter(price => price >= 20)
+	.reduce((accumulator, price) => accumulator + price);
 
-//filter
-let expensive = [];
-expensive = prices.filter((price) => price >= 20);
-console.log(`Expensive prices: ${expensive}`);
-
-//reduce
-let sum = 0;
-sum = expensive.reduce((accumulator, price) => accumulator + price);
-
-console.log(`Total: ${sum}`);
-
+console.log(sum);
